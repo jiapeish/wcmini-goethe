7ce41fdb8524ec8f86c31cfbe45ec4b82ea6d894
pejia <pejia@contextlogic.com>
2024-12-11 17:25:17 +0800

    add chat framework

--- pages/recommend/recommend.js
+++ pages/recommend/recommend.js
@@ -1,6 +1,32 @@
+// pages/recommend/recommend.js
 Page({
   data: {
+    userInput: '',
+    apiResponse: ''
   },
-  onLoad() {
+  handleInput(e) {
+    this.setData({
+      userInput: e.detail.value
+    })
+  },
+  sendMessage() {
+    wx.request({
+      url: 'https://openrouter.ai/',
+      method: 'POST',
+      data: {
+        text: this.data.userInput
+      },
+      success: (res) => {
+        this.setData({
+          apiResponse: res.data
+        })
+      },
+      fail: (err) => {
+        console.error('API request failed:', err)
+        this.setData({
+          apiResponse: 'API request failed'
+        })
+      }
+    })
   }
 })
